518af2cbb78d841017e65e8ad84a2d1c296b504b
AntonioCopete <antcopjim@gmail.com>
2021-10-19 17:51:06 +0200

    fixed system-requirements

--- src/pages/GameDetails.js
+++ src/pages/GameDetails.js
@@ -1,26 +1,27 @@
-import React, {useEffect, useReducer} from 'react';
-import Layout from '../components/Layout';
-import {useParams} from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useReducer } from "react";
+import Layout from "../components/Layout";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const initialState = {
-    currentGame: {}
-}
+  currentGame: {},
+  loadedGame: false,
+};
 
-function reducer(state, action){
-    switch(action.type){
-        case "LOAD_GAME": 
-            return {currentGame: action.payload}
-        default:
-            return state;
-    }
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOAD_GAME":
+      return { currentGame: action.payload, loadedGame: true };
+    default:
+      return state;
+  }
 }
 
 export default function GameDetails() {
-    const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const {gameId} = useParams();
-    console.log(gameId);
+  const { gameId } = useParams();
+  console.log(gameId);
 
   useEffect(() => {
     var options = {
@@ -35,44 +36,58 @@ export default function GameDetails() {
 
     axios.request(options).then(function (response) {
       console.log(response.data);
-      dispatch({type: "LOAD_GAME", payload: response.data});
+      dispatch({ type: "LOAD_GAME", payload: response.data });
     });
   }, [gameId]);
 
-  const {title, thumbnail, description, status, developer, genre, platform, publisher, game_url, release_date} = state.currentGame;
-  const {os, processor, storage, memory, graphics} = state.currentGame.minimum_system_requirements;
-  // console.log(minimum_system_requirements)
-    return (
-        <div>
-            <Layout>
-                {state.currentGame !== undefined && <div className="container">
-                    <h2>{title}</h2>
-                    <div className="row">
-                        <div className="col col-6">
-                            <img src={thumbnail} alt={title} />
-                        </div>
-                        <div  className="col col-6">
-                            <p>Genre: {genre}</p>
-                            <p>Platform: {platform}</p>
-                            <p>Status: {status}</p>
-                            <p>Launch date: {release_date}</p>
-                            <p>Publisher: {publisher}</p>
-                            <p>Developer: {developer}</p>
-                        </div>
-                    </div>
-                    <div>
-                        <p>{description}</p>
-                        <p>Official page: {game_url}</p>
-                        <ul>
-                            <li>Graphics: {graphics}</li>
-                            {/* <li>Memory: {minimum_system_requirements.memory}</li> */}
-                            {/* <li>OS: {minimum_system_requirements.os}</li> */}
-                            {/* <li>Processor: {minimum_system_requirements.processor}</li> */}
-                            {/* <li>Storage: {minimum_system_requirements.storage}</li> */}
-                        </ul>
-                    </div>
-                </div>}
-            </Layout>
-        </div>
-    )
+  const {
+    title,
+    thumbnail,
+    description,
+    status,
+    developer,
+    genre,
+    platform,
+    publisher,
+    game_url,
+    release_date,
+    minimum_system_requirements,
+  } = state.currentGame;
+  console.log(minimum_system_requirements);
+  return (
+    <div>
+      <Layout>
+        {state.currentGame === undefined && <div>ERROR</div>}
+        {state.loadedGame && (
+          <div className="container">
+            <h2>{title}</h2>
+            <div className="row">
+              <div className="col col-6">
+                <img src={thumbnail} alt={title} />
+              </div>
+              <div className="col col-6">
+                <p>Genre: {genre}</p>
+                <p>Platform: {platform}</p>
+                <p>Status: {status}</p>
+                <p>Launch date: {release_date}</p>
+                <p>Publisher: {publisher}</p>
+                <p>Developer: {developer}</p>
+              </div>
+            </div>
+            <div>
+              <p>{description}</p>
+              <p>Official page: {game_url}</p>
+              <ul>
+                <li>Graphics: {minimum_system_requirements.graphics}</li>
+                <li>Memory: {minimum_system_requirements.memory}</li>
+                <li>OS: {minimum_system_requirements.os}</li>
+                <li>Processor: {minimum_system_requirements.processor}</li>
+                <li>Storage: {minimum_system_requirements.storage}</li>
+              </ul>
+            </div>
+          </div>
+        )}
+      </Layout>
+    </div>
+  );
 }
