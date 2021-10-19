1c380c22cd401ccfd450f4ec5d2d0c07741e10ea
AntonioCopete <antcopjim@gmail.com>
2021-10-19 15:08:59 +0200

    fixed layout

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -4,8 +4,10 @@ export default function GameCard({ title, thumbnail }) {
   console.log(thumbnail);
   return (
     <div className="fw-bold col col-4">
-      {title}
-      <img src={thumbnail} alt={title} />
+      <div className="row">
+        <h4 className="text-center">{title}</h4>
+        <img src={thumbnail} alt={title} />
+      </div>
     </div>
   );
 }
