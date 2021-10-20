d369f86d44edc1113e590a0e943a6a7714d4674b
AntonioCopete <antcopjim@gmail.com>
2021-10-20 09:30:34 +0200

    added localstorage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Home from "./pages/Home";
 import { useEffect, useReducer } from "react";
 import { HomeContext } from "./context/HomeContext";
-import GameDetails from './pages/GameDetails';
+import GameDetails from "./pages/GameDetails";
 
 const initialState = {
   games: [],
@@ -22,21 +22,33 @@ function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    var options = {
-      method: "GET",
-      url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
-      params: { "sort-by": "alphabetical" },
-      headers: {
-        "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
-        "x-rapidapi-key": "23e233b049msh4485b68fa7318bdp11fd05jsn2ac4d49d92a1",
-      },
-    };
+    const localStorageItem = JSON.parse(localStorage.getItem("games"));
+    if (!localStorageItem) {
+      var options = {
+        method: "GET",
+        url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+        params: { "sort-by": "alphabetical" },
+        headers: {
+          "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+          "x-rapidapi-key":
+            "23e233b049msh4485b68fa7318bdp11fd05jsn2ac4d49d92a1",
+        },
+      };
 
-    axios.request(options).then(function (response) {
-      console.log(response.data);
-      dispatch({ type: "LOAD", payload: response.data });
-    });
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        dispatch({ type: "LOAD", payload: response.data });
+      });
+    } else {
+      dispatch({ type: "LOAD", payload: localStorageItem });
+    }
   }, []);
+
+  useEffect(() => {
+    if (state.games.length > 0) {
+      localStorage.setItem("games", JSON.stringify(state.games));
+    }
+  }, [state.games]);
 
   return (
     <Router>
--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,15 +1,15 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function GameCard({ title, thumbnail, id }) {
   // console.log(thumbnail);
   return (
-    <div className="fw-bold col col-4">
+    <div className="fw-bold col col-12 col-md-6 col-lg-4">
       <Link to={`game/${id}`}>
-      <div className="row">
-        <h4 className="text-center">{title}</h4>
-        <img src={thumbnail} alt={title} />
-      </div>
+        <div className="row">
+          <h4 className="text-center">{title}</h4>
+          <img src={thumbnail} alt={title} />
+        </div>
       </Link>
     </div>
   );
