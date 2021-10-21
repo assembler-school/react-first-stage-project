86495c9ee091ee2633895b4f12e4e72efa278aa4
AntonioCopete <antcopjim@gmail.com>
2021-10-21 19:05:27 +0200

    improving layout

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -3,11 +3,19 @@ import { Link } from "react-router-dom";
 
 export default function GameCard({ title, thumbnail, id }) {
   return (
-    <div className="fw-bold col col-12 col-md-6 col-lg-4 ">
+    <div className="fw-bold col col-12 col-md-6 col-lg-4 my-3 ">
       <Link className="text-decoration-none text-light" to={`game/${id}`}>
-        <div className="row border rounded-3 mb-4 py-3 bg-dark bg-gradient">
-          <h4 className="text-center ">{title}</h4>
-          <img className="img-rounded" src={thumbnail} alt={title} />
+        <div
+          style={{ height: "100%" }}
+          className="row border rounded-3 mb-4 py-3 bg-secondary"
+        >
+          <h4 className="text-center text-light">{title}</h4>
+          <img
+            style={{ borderRadius: "30px" }}
+            className="img-rounded"
+            src={thumbnail}
+            alt={title}
+          />
         </div>
       </Link>
     </div>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,8 +5,8 @@ import Header from "./Header";
 export default function Layout({ children }) {
   return (
     <div className="container-fluid text-center bg-dark p-5">
-      <div className="row justify-content-center">
-        <div className="col col-12">
+      <div className="row justify-content-center ">
+        <div className="col col-12" style={{ maxWidth: "1200px" }}>
           <Header />
           {children}
           <Footer />
