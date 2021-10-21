b56ef28fbc2d2aa1433182a5494dd3ade0fa4dd8
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-10-21 12:14:03 +0200

    demo data

--- CryptoWeb/src/utils/getCryptoList.js
+++ CryptoWeb/src/utils/getCryptoList.js
@@ -1,15 +1,20 @@
 import axios from "axios";
+import db from "dotenv";
 
+db.config();
+// db.connect({
+//   key: process.env.DB_KEY,
+// });
+
+const key = process.env.DB_KEY;
 const BASE_URL =
   "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
-
-const HEADERS = {};
 
 export default async function getCryptoList() {
   try {
     const res = await axios.get(`${BASE_URL}?limit=10`, {
       headers: {
-        "X-CMC_PRO_API_KEY": "5da150cd-3557-4a3d-9b07-7bf7b57e96dd",
+        "X-CMC_PRO_API_KEY": { key },
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
         "Access-Control-Allow-Headers":
